87e48c28473d75cdf7272934f454b038c6fe06ab
ChaedongIm <chaedong.im.dev@gmail.com>
2024-10-16 00:11:41 +0900

    feat: errorBoundary 설정

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {
   QueryClientProvider,
   useQuery,
 } from "@tanstack/react-query";
+import { ErrorBoundary } from "@suspensive/react";
 
 const queryClient = new QueryClient({ defaultOptions: {} });
 
@@ -19,31 +20,38 @@ function App() {
 function Page() {
   return (
     <div className="flex flex-col">
-      <Suspense fallback={<div>waiting 100...</div>}>
-        <Component wait={100} />
-      </Suspense>
-      <Suspense fallback={<div>waiting 500...</div>}>
-        <Component wait={500} />
-      </Suspense>{" "}
-      <Suspense fallback={<div>waiting 1000...</div>}>
-        <Component wait={1000} />
-      </Suspense>
+      <ErrorBoundary fallback={<div>error...1</div>}>
+        <Suspense fallback={<div>waiting 100...</div>}>
+          <Component wait={100} error />
+        </Suspense>
+      </ErrorBoundary>
+      <ErrorBoundary fallback={<div>error...2</div>}>
+        <Suspense fallback={<div>waiting 500...</div>}>
+          <Component wait={500} />
+        </Suspense>
+      </ErrorBoundary>
+      <ErrorBoundary fallback={<div>error...3</div>}>
+        <Suspense fallback={<div>waiting 1000...</div>}>
+          <Component wait={1000} />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
 
-function Component({ wait }: { wait: number }) {
+function Component({ wait, error }: { wait: number; error?: boolean }) {
   const query = useQuery({
     queryKey: ["fetching", wait],
-    queryFn: () => fetching({ wait, error: false }),
+    queryFn: () => fetching({ wait, error }),
     suspense: true,
+    useErrorBoundary: true,
   });
   return <div>{query.data}</div>;
 }
 
 export default App;
 
-async function fetching({ error, wait }: { error: boolean; wait: number }) {
+async function fetching({ error, wait }: { error?: boolean; wait: number }) {
   await new Promise((resolve, reject) => {
     if (error) reject();
     setTimeout(resolve, wait);
